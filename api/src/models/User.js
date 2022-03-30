57387e2bc3a027ce4745ecad26f971f1e5ec07f6
TomasLaus <tomaslaus99@gmail.com>
2022-03-30 20:05:47 -0300

    Update User.js
    Se agrego el gender al modelo User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -9,7 +9,10 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
