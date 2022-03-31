b3bdf7e811ada18681e5adbd1a3920c0e0521819
maxisorichetti <maxisorichetti19@gmail.com>
2022-03-31 12:06:47 -0300

    Create Login

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Register } from '../features/Register';
+import { Login } from '../features/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
