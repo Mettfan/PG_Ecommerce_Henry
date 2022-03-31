ea8796e98ad9a3a803a6c0336a1794b98be185ec
RoxanaCanciani <roxicanciani@gmail.com>
2022-03-31 11:36:15 -0300

    ruta por id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,6 +9,7 @@ const router = Router();
 const postUser = require('./user.js');
 
 
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
@@ -18,6 +19,25 @@ router.get('/productos', async(req,res)=>{
     res.send(apiInfo);
     })
 
+router.get('/producto/:id', async(req,res)=>{
+    const {id} = req.params
+    const apiInfo= await getInfo();
+    try {
+        if (id) {
+          let productId = await apiInfo.filter((el) => el.id === parseInt(id)
+          );
+       
+          productId.length? res.status(200).send(productId)
+            : res.status(400).send("Not found");
+        }
+      } catch (err) {
+        res.json({ message: err });
+      }
+    })  
+
+  
+
 router.use('/crearusuario', postUser);
+
 
 module.exports = router;
