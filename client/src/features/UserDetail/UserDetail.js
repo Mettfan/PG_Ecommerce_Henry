2d622c75aece0a34492af384fa0488e3c27fd3f8
Mettfan <cesaryannick6@gmail.com>
2022-04-07 15:49:08 -0500

    User Detail Compatible & Ruta Add Shopping en back

--- client/src/features/UserDetail/UserDetail.js
+++ client/src/features/UserDetail/UserDetail.js
@@ -1,17 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import { useSelector } from "react-redux"
 import './UserDetail.css'
 
 export default function UserDetail (props){
     const { logout, isAuthenticated, user } = useAuth0()
 
+    let userValidated = useSelector( state => state.userReducer.status.user )
+    let isUserAuthenticated = isAuthenticated || userValidated
     return (<>
 
 
+            {JSON.stringify(userValidated) + 'status'}
         
-        {isAuthenticated && <div>
-            <img  className="userImgOnprofile" src={user.picture}></img>
-            <div>{user.name} </div>
-            <div>{user.email} </div>
+        {isUserAuthenticated && <div>
+            <img  className="userImgOnprofile" src={user?.picture || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL3-fxYXhHbPLtDz72SAnRopI8b22xxS-SHCNTp8VpPP8GuOD4Ix3kxB3OokobuqGctVE&usqp=CAU" }></img>
+            <div>{user?.name || userValidated.name } </div>
+            <div>{user?.email || userValidated.email} </div>
             {/* {JSON.stringify(user)} */}
             <button className="logoutbtn" onClick={ () => logout()}>LOGOUT</button>
             
