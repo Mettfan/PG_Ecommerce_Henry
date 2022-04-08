385553c3f716df6d597d55d3130ebf02514e6814
Mettfan <cesaryannick6@gmail.com>
2022-04-08 08:06:09 -0500

    V1.044UNFINISHED

--- client/src/features/ShoppingCart/ShoppingCart.js
+++ client/src/features/ShoppingCart/ShoppingCart.js
@@ -1,14 +1,18 @@
+import { useEffect } from "react"
 import { useSelector } from "react-redux"
 
 export default function ( ) {
+    useEffect( ( )=> {
+        console.log( 'GETTING SHOPPING LIST')
+     }, [])
     
 
     let state = useSelector( state => state.shoppingCartReducer  )
     return (<>
     
         <b>HERE IS YOUR SHOPPING CART!</b>
-        {JSON.stringify(state)}
-        {console.log(state)}
+        {/* {JSON.stringify(state)}
+        {console.log(state)} */}
 
     
     </>)
