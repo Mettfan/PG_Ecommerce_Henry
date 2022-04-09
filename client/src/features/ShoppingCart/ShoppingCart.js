fb7d061cf10fe9f64ec0053687bc86769fd9d11d
TomasLaus <tomaslaus99@gmail.com>
2022-04-08 23:42:59 -0300

    Carrito
    Se agregan productos al carrito

--- client/src/features/ShoppingCart/ShoppingCart.js
+++ client/src/features/ShoppingCart/ShoppingCart.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import {getShoppingList} from '../../redux/actions/shoppingCartActions'
-export default function ( ) {
+export default function ShoppingCart ( ) {
     const status = useSelector( state => state )
     const user  = status.userReducer
     const shopping = status.shoppingCartReducer
@@ -14,21 +14,29 @@ export default function ( ) {
         
         // dispatch(getShoppingList({ email: user.status.user.email }))
 
-        axios.get('http://localhost:3001/usuario/shopping', { email: user.status.user.email }).then( response => {
+        const productosCarrito = axios.get('http://localhost:3001/usuario/shopping', { email: user.status.user.email }).then( response => {
             console.log(response.data)
+            console.log(status.shoppingCartReducer)
         })
 
 
-
+        return productosCarrito
      }, [])
     
-
+     const ProductosParaMostrar = status.shoppingCartReducer.productos.msg
     // let state = useSelector( state => state.shoppingCartReducer  )
+    console.log(ProductosParaMostrar)
     return (<>
     
         <b>HERE IS YOUR SHOPPING CART!</b>
         {JSON.stringify(user.status.user.email)}
-        {JSON.stringify(status.shoppingCartReducer)}
+        {ProductosParaMostrar.map(producto => 
+                <div>
+                <p>{producto.name}</p>
+                <p>${producto.price}</p>
+                <img src={producto.image} alt="producto" width="50px" height="50px" />
+            </div>)
+        }
         {/* {JSON.stringify(state)}
         {console.log(state)} */}
 
