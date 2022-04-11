bfef426a1736cfab6ca14292ff44377d81cf153a
Mettfan <cesaryannick6@gmail.com>
2022-04-11 01:01:07 -0500

    Demo 2 Estable

--- client/src/features/UserFavorites/UserFavorites.js
+++ client/src/features/UserFavorites/UserFavorites.js
@@ -1,7 +1,9 @@
+import './UserFavorites.css'
 export default function ( ) {
     return (<>
     
-        <b>HERE ARE YOUR FAVORITE PRODUCTS!</b>
-    
+        {/* <b>HERE ARE YOUR FAVORITE PRODUCTS!</b> */}
+        <h1 className="comingsoon">A dev team is working hard on this feature!  <h5><i>Come back soon...</i></h5> </h1>
+        <img className='devimg' src='https://t3.ftcdn.net/jpg/02/24/86/92/360_F_224869243_WZHOVD3GqYq6ZUzRkZbQNTXzg1FzHwhd.jpg' alt=''></img>
     </>)
 }
