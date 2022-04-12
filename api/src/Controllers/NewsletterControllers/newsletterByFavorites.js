15d58cb90379730f5bf38ddca558268fa7af351c
Guillermo Jofre <jofregf@gmail.com>
2022-04-12 13:13:37 -0300

    Update newsletterByFavorites.js

--- api/src/Controllers/NewsletterControllers/newsletterByFavorites.js
+++ api/src/Controllers/NewsletterControllers/newsletterByFavorites.js
@@ -1,42 +1,6 @@
-// const mailchimp = require("@mailchimp/mailchimp_marketing");
-// const md5 = require("md5");
-// const listId = process.env.ID_LIST_MAILCHIMP;
+
 const {User} = require('../../db.js');
-// const axios = require ('axios');
-// const getRequestParams = require('../../helpers/configNewsletter')
 
-
-// const newsProductFavorite = async (req, res, next) => {
-//     const {email} = req.body;
-//     if (!email) return res.status(404).json({msg:"Por favor ingrese un email correcto"});
-//     try {
-//         const user = await User.findOne({where: {email}});
-//         if(!user) {
-//             res.status(404).json({msg: 'Usuario no encontrado'});
-//         } else {
-//             const products = await user.getProducts();
-//             // console.log(products, '<--')
-//             const name = products.map(product => product.name); 
-//             // console.log(name, 'name')
-//             const {url, data, headers} = getRequestParams(email);
-//             await axios.post(url, data, {headers});
-            
-                
-//                 const subscriberHash = md5(email.toLowerCase());
-//                 await mailchimp.post(`lists/${listId}/members/${subscriberHash}/tags`, {
-//                     tags: [{"name": "mierda", "status": "active"}]
-//                 })
-                
-//             }
-//             res.status(200).json({msg: 'Subscripto a noticias de sus productos favoritos'});
-//             console.log(response, 'response');
-        
-//     } catch (error) {
-//         next(error);
-//     }
-    
-
-// }
 const Mailchimp = require('mailchimp-api-v3');
 var md5 = require('md5');
 
@@ -52,35 +16,36 @@ const isEmptyMailchimp = typeof mailchimp === Object && mailchimp.keys(mailchimp
 
 const newsProductFavorite = (req, res, data) => {
     const {email} = req.body;
-
-    if (!mailchimp || !email || isEmptyMailchimp || !ID_LIST_MAILCHIMP) {
-        const msg = `No puede suscribirse, perdida del parametro ${!email ? 'email': 'API Key or List ID'}`;
-        console.warn(msg);
-        return Promise.reject({ msg });
+    try {
+        if (!mailchimp || !email || isEmptyMailchimp || !ID_LIST_MAILCHIMP) {
+            return res.status(404).json({msg: `No puede suscribirse, perdida del parametro ${!email ? 'email': 'API Key or List ID'}`});
+        }
+        mailchimp.post(`lists/${ID_LIST_MAILCHIMP}`, {
+       
+            members: [{
+                email_address: email.toLowerCase(),
+                status : data.status || 'subscribed',
+                merge_fields: {},
+            }],
+    
+        }).then(m => {
+            if (m && Object.keys(m.errors).length > 0) {
+                console.log('Error adding new subscriber to MC', m.errors);
+                return Promise.reject({ m });
+            }
+            addTag(email)
+            return Promise.resolve({ m });
+            
+        })
+        res.status(200).json({msg: 'Suscrito correctamente para recibir noticias de su producto favorito'});
+    } catch (error) {
+        console.log(error);
     }
 
-    return mailchimp.post(`lists/${ID_LIST_MAILCHIMP}`, {
-       
-        members: [{
-            email_address: email.toLowerCase(),
-            status : data.status || 'subscribed',
-            merge_fields: {},
-        }],
+    
 
-    }).then(m => {
-        if (m && Object.keys(m.errors).length > 0) {
-            console.log('Error adding new subscriber to MC', m.errors);
-            return Promise.reject({ m });
-        }
-        addTag(email)
-        return Promise.resolve({ m });
-        
-    }).catch(err => {
-        console.warn('Failed adding subscriber', email, err);
-        return Promise.reject({ err });
-    });
+   
 }
-
 
 const addTag = async (email) => {
 
