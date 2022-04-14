65dd5e6020764d40a654a2b6cde5b2ec055495d0
Cesar Yannick Torres <cesaryannick6@gmail.com>
2022-04-14 08:12:01 -0500

    Revert "Revert "Modificacion modelo product""

--- api/src/Controllers/ProductControllers/postProduct.js
+++ api/src/Controllers/ProductControllers/postProduct.js
@@ -1,6 +1,6 @@
 const { Product, Category } = require('../../db.js')
 const postProduct = async(req,res, next )=>{
-    const {name, description, size, color, gender, stock, price, descount, image, category} = req.body
+    const {name, description, size, color, gender, stock, price, discount, warranty, brand, suitable_for, composition, origin, important_data, extras, disabled, image, category} = req.body
     console.log(name, typeof name)
     console.log(description, typeof description)
     console.log(size, typeof size)
@@ -12,7 +12,7 @@ const postProduct = async(req,res, next )=>{
 
     try{
 
-        let productCreated = await Product.create({ name: name, description: description, size: size, color: color, gender: gender, stock: stock, price: price, descount: descount, image: image, category: (category || 'General') });
+        let productCreated = await Product.create({ name: name, description: description, size: size, color: color, gender: gender, stock: stock, price: price, discount: discount, warranty, brand, suitable_for, composition, origin, important_data, extras, disabled, image: image, category: (category || 'General') });
         
 
         try{
