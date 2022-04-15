fb194667a54c68111e6e8c4f6426c0444f15cbf4
Mettfan <cesaryannick6@gmail.com>
2022-04-15 12:33:28 -0500

    Conectar cantidad con pago

--- client/src/features/ShoppingCart/ShoppingCart.js
+++ client/src/features/ShoppingCart/ShoppingCart.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import {getShoppingList} from '../../redux/actions/shoppingCartActions'
 import CardSlim from "../../components/CardSlim/CardSlim"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import './ShoppingCart.css'
 import Cookies from "universal-cookie"
 
@@ -16,7 +16,7 @@ export default function ShoppingCart ( ) {
     // let userValidated = useSelector( state => state.userReducer.status.user )
     let userValidated = cookie.get('user').user
     //let isUserAuthenticated = isAuthenticated || userValidated
-    
+    let nav = useNavigate()
     const status = useSelector( state => state )
     const userRed  = status.userReducer
     const shopping = status.shoppingCartReducer
@@ -84,6 +84,10 @@ export default function ShoppingCart ( ) {
         // console.log('total useEffect', total)
         //     // dispatch(pruebaAction())
         // })
+    let setShoppingTotal = ( ) => {
+        cookie.set('total', total)
+        nav("/user/products/pay")
+    }
     return (<>
     
         {/* <b>HERE IS YOUR SHOPPING CART!</b>
@@ -171,9 +175,9 @@ export default function ShoppingCart ( ) {
                             <p>${ total }</p>
                         </div>
                     </div>
-                        <Link to="/user/products/pay">
+                        <button onClick={() => setShoppingTotal() }>
                             <button className="btn-continue-cart" onClick={() => handleContinue()} >Continuar</button>
-                        </Link>
+                        </button>
                 </div>
         </div>
     </div>
