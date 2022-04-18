8954652760e944570034fe3e6498dbf36c0f09d7
Mettfan <cesaryannick6@gmail.com>
2022-04-18 14:22:08 -0500

    Fav Fixed

--- client/src/features/ProductDetail/ProductDetail.js
+++ client/src/features/ProductDetail/ProductDetail.js
@@ -1,164 +1,123 @@
-
 import {MdOutlineArrowBack} from 'react-icons/md'
 import { BsSuitHeartFill } from 'react-icons/bs';
 import { Link, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
-import { getProduct } from "../../redux/actions/productActions"
+import { getProduct, deleteProductAction } from "../../redux/actions/productActions"
 import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
 import './index.css'
+
+import { addProductFavorite } from '../../redux/actions/favoriteActions';
+import { addProduct } from '../../redux/actions/shoppingCartActions';
+
 import Cookies from 'universal-cookie';
 
+
 export default function ProductDetail (props) {
-
-  const {  isAuthenticated, user  } = useAuth0()
+  let { id } = useParams()
   let cookie = new Cookies()
+  let dispatch = useDispatch()
+  let nav = useNavigate()
+  let product = useSelector( (state) => state.productReducer.producto)
+  let userValidated = useSelector( state => state.userReducer.status.user ) || cookie.get('user').user 
+  // let statusFav = useSelector( state => state.favoriteReducer.status )
+  const {  isAuthenticated, user  } = useAuth0()
 
-
-  const [show, setShow] = useState(false)
-
-  let userValidatedSelector = useSelector( state => state.userReducer.status.user )
-
-  // let userValidated = useSelector( state => state.userReducer.status.user )
-  let userValidated = cookie.get('user').user
-
+  // let userValidated = 
   let isUserAuthenticated = isAuthenticated || userValidated
-  
+  let usuario = userValidated || user
 
-    
-    let { id } = useParams()
-    let dispatch = useDispatch()
+  // console.log('userValidated', userValidated)
+
     useEffect(() => {
       if(id) {
-        
         dispatch(getProduct(id))
       }
+      return function deleteProduct(){
+        dispatch(deleteProductAction())
+      }
     }, [])
-    let product = useSelector( (state) => state.productReducer.producto)
     
     product = product ? product : props.producto
-    let nav = useNavigate()
-    async function addShoppingCart  (){ 
 
-      if (!isUserAuthenticated) {
-        nav('/login')
-      } else {
-
-        let usuario = userValidatedSelector || user
-        console.log("ASOCIANDO: "+usuario?.email)
-        console.log('CON '+id)
-        await axios.post('http://localhost:3001/usuario/shopping', { productId: Number(id), userEmail: usuario?.email}).then( response => {
-          console.log(response.data)
-          dispatch({ type: 'ADD_PRODUCT', payload: response.data })
-        },
-        (error) => console.log(error))
-        setShow(true)
-      }
-
-
-
+    const addShoppingCart = () => { 
+      dispatch(addProduct({ productId: Number(id), userEmail: usuario?.email}))
     }
+
+    const addFavorites = () => { 
+      dispatch(addProductFavorite({ productId: Number(id), email: usuario?.email}))
+      // nav(!isUserAuthenticated?'../login':'../user/favorite')
+    }
+
     return (<>
-        {product ? 
-    
-      
-    <div className="main-container">
-    <div className="detailProduct-container">
-      <div className="gender-category">
-        <p> {product.gender +'/'+product?.CategoryName}  </p>
-        {console.log(product)}
-      <Link to="/home" >
-          <MdOutlineArrowBack />
-        </Link>
-      </div>
-      <div className="detail-one">
-        <div className="detail-one-left">
-          {/* <div className="detail-img"> */}
-            <img className="detail-img" src={product.image} alt="zapatilla" />
-            {/* </div> */}
-        </div>
-        {/* <div></div> */}
-        {/* <div className="detail-arrow"></div> */}
-        <div className="detail-one-right">
-          <h1 className="detail-one-name"> {product.name} </h1>
-          <div className="detail-one-sku">SKU: Item No. NI_{product.id}  </div>
-          <div className="detail-one-price">${product.price} </div>
-          <div className="detail-one-cards">Medios de pago</div>
-          <div className="detail-one-size">
-            <p className="detail-size">Talles</p>
-            
-              {product?.size?.split("-")?.map(el => <strong key={el}> <div className="detail-sizes2">{el}</div>     </strong>)}
-            
+        {product 
+          ? 
+            <div className="main-container">
+              <div className="detailProduct-container">
+                <div className="gender-category">
+                  <p> {product.gender +'/'+product?.CategoryName}  </p>
+                  <div className="arrowBack">
+                    <Link to="/home" >
+                      <MdOutlineArrowBack />
+                    </Link>
+                  </div>
+                </div>
+                <div className="detail-one">
+                  <div className="detail-one-left">
+                      <img className="detail-img" src={product.image} alt="zapatilla" />
+                  </div>
+                  <div className="detail-one-right">
+                    <h1 className="detail-one-name"> {product.name} </h1>
+                    <div className="detail-one-sku">SKU: Item No. NI_{product.id}  </div>
+                    <div className="detail-one-price">${product.price} </div>
+                    <div className="detail-one-cards">Medios de pago</div>
+                    <div className="detail-one-size">
+                      <p className="detail-size">Talles</p>
+                      
+                        {product?.size?.split("-")?.map(el => <strong key={el}> <div className="detail-sizes2">{el}</div>     </strong>)}
+                      
+                      </div>
+                    <div className="detail-one-buttons">
+                      <button onClick={ () => addShoppingCart()} className="detail-button-buy">Agregar al carrito</button>
+                      <button onClick={ () => addFavorites()} style={{border: 'none', background: 'none', textDecoration: 'none' }}>
+                        <BsSuitHeartFill className="detail-button-like"/>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="detail-two">
+                  <div className="description-left">
+                      <strong>Descripción</strong>
+                  </div>
+                  <div className="description-right">
+                    <p className="description-top-detail"> {product.description}</p> 
+                    <div className="description-bottom">
+                      <div className="description-bottom-p">
+                        <strong>Género:</strong> {product.gender} <br />
+                        <strong>Beneficios:</strong> Amortiguación<br />
+                        <strong>Caña:</strong> Baja<br />
+                        <strong>Importante:</strong> El peso del calzado puede variar según el número solicitado. Los talles corresponden a numeración de Argentina<br />
+                        <strong>Origen:</strong> Importado<br />
+                      </div>
+                      <div className="description-bottom-p">
+                        <strong>Adecuado para:</strong> Todo el día <br />
+                        <strong>Composición:</strong> Capellada<br /> 
+                        <strong>Malla/Sintético / Suela</strong>: Goma<br />
+                        <strong>Ajuste:</strong> Con Cordones<br />
+                        <strong>Garantía:</strong> Contra defecto de fabricación<br />
+                        <strong>Marca:</strong> Nike<br />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          <p className={show ? 'producto_agregado' : 'producto_sinagregar'}> 🟢 El producto fue agregado al carrito</p>
-          <div className="detail-one-buttons">
-          {/* <Link to="/login" style={{ textDecoration: 'none' }}> */}
-          <button onClick={ () => addShoppingCart()} className="detail-button-buy">Agregar al carrito</button>
-            {/* </Link> */}
-            
-            <Link to="/home" style={{ textDecoration: 'none'}} className="detail-button-like">
-            <button style={{border: 'none', background: 'none', textDecoration: 'none' }}>
-              <BsSuitHeartFill className="detail-button-like"/>
-            </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="detail-two">
-        <div className="description-left">
-            <strong>Descripción</strong>
-        </div>
-        <div className="description-right">
-          <p className="description-top"> {product.description}</p> 
-          <div className="description-bottom">
-            <div className="description-bottom-p">
-              <strong>Género:</strong> {product.gender} <br />
-              <strong>Beneficios:</strong> Amortiguación<br />
-              <strong>Caña:</strong> Baja<br />
-              <strong>Importante:</strong> El peso del calzado puede variar según el número solicitado. Los talles corresponden a numeración de Argentina<br />
-              <strong>Origen:</strong> Importado<br />
-            </div>
-            <div className="description-bottom-p">
-              <strong>Adecuado para:</strong> Todo el día <br />
-              <strong>Composición:</strong> Capellada<br /> 
-              <strong>Malla/Sintético / Suela</strong>: Goma<br />
-              <strong>Ajuste:</strong> Con Cordones<br />
-              <strong>Garantía:</strong> Contra defecto de fabricación<br />
-              <strong>Marca:</strong> Nike<br />
-            </div>
-          </div>
-        </div>
-        </div>
-      <div className="detail-three"></div>
-      <div className="detail-four"></div>
-    </div>
-  </div>
-  
-  :
-  
-  <h2>Loading</h2>
-  
-}
+          :
+          <h2>Loading</h2>
+        }
     
     
-    </>)
-
-    // return (<div>
-    //     PRODUCT DETAIL { ' '+ id}
-    //     {console.log(product)}
-    //     <div className="detail-one-name">{product?.name}</div>
-    //     <div className="detail-one-description">{product?.description}</div>
-    //     <div>{product?.size}</div>
-    //     <div>{product?.color}</div>
-    //     <div>{product?.gender}</div>
-    //     <div>{product?.stock}</div>
-    //     <div>{product?.price}</div>
-    //     <img src={product?.image}></img>
-
-    //     <div>
-            
-    //     </div>
-    // </div>)
+    </>
+  )
 }
--- client/src/redux/actions/favoriteActions.js
+++ client/src/redux/actions/favoriteActions.js
@@ -6,53 +6,54 @@ export const DELETE_PRODUCT_FAVORITE = 'DELETE_PRODUCT_FAVORITE'
 export const ERROR = 'ERROR'
 
 export const addProductFavorite = ( {productId, email} ) => async dispatch  => {
-  console.log('productId, userEmail', productId, email)
-    await axios.post('http://localhost:3001/usuario/favorites', {
-        productId,
-        email
-    }).then( response => {
-      console.log('response.data action fav', response.data)
-        dispatch({
-            type: ADD_PRODUCT_FAVORITE,
-            payload: response.data
-        })
-    },
-    (error) => {
-        dispatch({
-            type: ERROR,
-            payload: error.error
-        })
-    })
-}
+    console.log('productId, userEmail', productId, email)
+      await axios.post('http://localhost:3001/usuario/favorites', {
+          productId,
+          email
+      }).then( response => {
+        console.log('response.data action fav', response.data)
+          dispatch({
+              type: ADD_PRODUCT_FAVORITE,
+              payload: response.data
+          })
+      },
+      (error) => {
+          dispatch({
+              type: ERROR,
+              payload: error.error
+          })
+      })
+  }
 
-export const getFavorites = ( email ) => async dispatch => {
-  console.log('getFavoritesAction email', email)
-    await axios.get(`http://localhost:3001/usuario/favorites/${email}`).then( response => { console.log('response.data getFavAct', response.data)
-        dispatch({
-            type: GET_FAVORITE,
-            payload: response.data.products
-        })
-    },
-    (error) => {
-        dispatch({
-            type: ERROR,
-            payload: error.error
-        })
-    })
-}
 
-export const deleteProductFavorite = ( {productId, email} ) => async ( dispatch ) => {
-    axios.delete(`http://localhost:3001/usuario/favorites/${email}/${productId}`).then( response => {
-      console.log('response delete fav', response.data)
-        dispatch({
-            type: DELETE_PRODUCT_FAVORITE,
-            payload: response.data.favorite
-        })
-    },
-    (error) => {
-        dispatch({
-            type: ERROR,
-            payload: error.error
-        })
-    })
-}
+  export const getFavorites = ( email ) => async dispatch => {
+    console.log('getFavoritesAction email', email)
+      await axios.get(`http://localhost:3001/usuario/favorites/${email}`).then( response => { console.log('response.data getFavAct', response.data)
+          dispatch({
+              type: GET_FAVORITE,
+              payload: response.data.products
+          })
+      },
+      (error) => {
+          dispatch({
+              type: ERROR,
+              payload: error.error
+          })
+      })
+  }
+  
+  export const deleteProductFavorite = ( {productId, email} ) => async ( dispatch ) => {
+      axios.delete(`http://localhost:3001/usuario/favorites/${email}/${productId}`).then( response => {
+        console.log('response delete fav', response.data)
+          dispatch({
+              type: DELETE_PRODUCT_FAVORITE,
+              payload: response.data.favorite
+          })
+      },
+      (error) => {
+          dispatch({
+              type: ERROR,
+              payload: error.error
+          })
+      })
+  }
