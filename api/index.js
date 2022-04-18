4cf7b4ac2defe1640f47bf1f8d8597dd128722f1
Mettfan <cesaryannick6@gmail.com>
2022-04-18 11:48:02 -0500

    Delete good

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const preloader = require('./src/preloader');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
 
     preloader();
