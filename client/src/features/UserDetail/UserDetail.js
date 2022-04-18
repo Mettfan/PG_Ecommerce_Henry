964eba2c5283bd9dd6b1a9dcdb58d0d2ed43bb40
Mettfan <cesaryannick6@gmail.com>
2022-04-18 07:23:39 -0500

    Minor changes

--- client/src/features/UserDetail/UserDetail.js
+++ client/src/features/UserDetail/UserDetail.js
@@ -62,7 +62,7 @@ export default function UserDetail(props) {
 
                     <Link to="/order">
                         <button className="logoutbtn">Mi compra</button>
-
+                        </Link>
                     <Link to="/order/finder">
                         <button >Mi compra</button>
 
