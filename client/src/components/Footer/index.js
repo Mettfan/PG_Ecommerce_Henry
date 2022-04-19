79713e217ca9e8ac400d860a844a41631bb36044
leonardomontilla74 <leme.310589@gmail.com>
2022-04-19 16:19:56 -0400

    Terminos/facebook/instagram

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './index.css';
 import { Link } from 'react-router-dom';
+import facebook from '../../assets/social-facebook.png';
+import instagram from '../../assets/social-instagram.png'
 
 function Footer() {
   return (
@@ -8,15 +10,26 @@ function Footer() {
           <div className="footer-container">
             <h1>Booma </h1>
             <div>
-          <Link to={'/terminos'}><p>Terminos y condiciones</p></Link>
+          <Link to={'/terminos'} className='link-footer' >
+            <p>Terminos y condiciones</p>
+          </Link>
             </div>
         <div>
               <p>Preguntas frecuentes</p>
             </div>
-            <div>
-          <a href="https://www.facebook.com/profile.php?id=100080195515452"><p>Facebook</p></a>
-          <a href="http://www.instagram.com"><p>Instagram</p></a>
-            </div>
+        <div className='footer-container'>
+          <a className='link-footer'
+            href="https://www.facebook.com/profile.php?id=100080195515452">
+            <p>Facebook</p>
+            <img src={facebook} alt=" " width={30} />
+          </a>
+        </div>
+        <div className='footer-container'>
+          <a className='link-footer'
+            href="http://www.instagram.com/boomaropadeportiva"><p>Instagram</p>
+            <img src={instagram} alt=" " width={30} />
+          </a>
+        </div>
             <div >
               <p>Contacto</p>
               <Link to="/map" className='link-footer'>
