8d5e4cf2d975e928b0c9d2f556845a94d706aea0
Mettfan <cesaryannick6@gmail.com>
2022-04-18 19:41:24 -0500

    Order Finished

--- client/src/features/UserOrderView/UserOrderView.js
+++ client/src/features/UserOrderView/UserOrderView.js
@@ -100,8 +100,8 @@ export default function OrderFinder()  {
                 </div>
                  {/* El boton que te redirige al ticket */}
                   <div>
-                {state.orderFound.status === 'pending' && <button onClick={( ) => window.location.href = state.orderFound.transaction_details?.external_resource_url }> Ticket</button>  }
-
+                {/* {state.orderFound.status === 'pending' && <button onClick={( ) => window.location.href = state.orderFound.transaction_details?.external_resource_url }> Ticket</button>  } */}
+                {<a  target={'_blank'} href={state.orderFound.transaction_details?.external_resource_url} download= 'Ticket'>download ticket</a>}
                 </div>
    
     {/* <div>
