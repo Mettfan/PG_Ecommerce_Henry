cf5e10ee2cc737e10639e48f43e7990646eb501a
TomasLaus <tomaslaus99@gmail.com>
2022-04-21 08:43:39 -0300

    Adios reviews

--- client/src/features/ProductDetail/ProductDetail.js
+++ client/src/features/ProductDetail/ProductDetail.js
@@ -181,52 +181,8 @@ export default function ProductDetail (props) {
         </div>
         </div>
       <div >
-        <h1 className="detail-three-titleh1">VALORACIONES Y RESEÑAS</h1>
       </div>
-            <p>Valoración general</p>
-            <div className="detail-three-comments">
-             <p className="detail-three-valoracion">★ ★ ★ ★ ✩ 4.0 </p>
-
-            </div>
-            {
-              reviewsOfTheProduct && reviewsOfTheProduct.length != 0 ?
-              <>
-              <h2 className="comentariosTitulo">Comentarios</h2>
-             
-              {
-                
-                
-                reviewsOfTheProduct?.map(review => (
-                  <div className="divComments">
-                    
-                    <strong> {review?.User?.name} </strong>
-                    <strong> {review?.User?.lastName} </strong>
-                    {
-                    review?.rating == 1 ? <p>★ ✩ ✩ ✩ ✩</p> 
-                    : 
-                    review?.rating == 2 ? <p>★ ★ ✩ ✩ ✩</p> 
-                    :
-                    review?.rating == 3 ? <p>★ ★ ★ ✩ ✩</p> 
-                    :
-                    review?.rating == 4 ? <p>★ ★ ★ ★ ✩</p> 
-                    :
-                    review?.rating == 5 ? <p>★ ★ ★ ★ ★</p> 
-                    :
-                    null
-                    
-                    }
-                    <span>  </span>
-                    <p> {review?.comment} </p>
-                    
-                  </div>
-                ))
-              }
-             
-             
-              </>
-              :
-              <h3  style={{ margin: '5% auto'}}>¡Sé el primero en hacer una reseña!</h3>
-            }
+         
       <div className="detail-four">
 
       </div>
--- client/src/features/UserOrderView/UserOrderView.js
+++ client/src/features/UserOrderView/UserOrderView.js
@@ -123,32 +123,8 @@ export default function OrderFinder()  {
     
       {/* <div>-----------------------------------------------</div> */}
                 </div>
-                <div className='reviewDivOrder'>   
+                
                      
-                     <div class="txt-center">
-                        <form>
-                     <label for="titleReview">Resumen de tu resena</label>
-                     <input type="text" id="titleReview" name="reviewTitle" />
-                            <div class="rating">
-                     rating
-                                <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
-                                <label for="star5">☆</label>
-                                <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
-                                <label for="star4">☆</label>
-                                <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
-                                <label for="star3">☆</label>
-                                <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
-                                <label for="star2">☆</label>
-                                <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
-                                <label for="star1">☆</label>
-                                <div class="clear"></div>
-                            </div>
-                     <label for="titleDescripcion">Descripcion</label>
-                     <input type="text" id="titleDescripcion" name="reviewDescripcion" />
-                        </form>
-                    </div>
-                     
-             </div>
              </div>
              )
 
